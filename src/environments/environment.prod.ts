73aeef69056a4e897c8e1fc3e7afd19929f52600
Mateusz Ciezczak <m0686@student.jamk.fi>
2018-09-30 23:24:34 +0300

    add(): custom nginx conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'http://solteq-assignment.mciezczak.pl:9000/api',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'pl-PL']
 };
